b87d8e85836b78f0290e80aab9f3b10cdaa3cd2d
Dmitriy Morozov <morozov801@gmail.com>
2020-07-03 20:34:48 +0300

    first commit with pythonfile

---
+++ Test/Strings.py
@@ -0,0 +1,25 @@
+# This script by Dmitriy Morozov
+#
+#   _________________________________
+#   Version 1
+#
+#   2020
+#
+#   _________________________________
+
+name_f = "Dmitriy"
+name_l = "Morozov"
+print ((name_f + " " + name_l)*3)
+
+print (name_f[0:6:2])
+
+print(name_f.split('i'))
+
+i=0
+while i < 10:
+   print ("i = " + str(i))
+   i += 1
+   if i==5:
+       break
+
+
---
+++ Test/Testfile.py
@@ -0,0 +1,25 @@
+# This script by Dmitriy Morozov
+#
+#   _________________________________
+#   Version 1
+#
+#   2020
+#
+#   _________________________________
+
+
+a = 34
+t = "Dmitriy Morozov"
+
+print(a)
+print(t)
+
+print("This is " + t + " and he old is " + str(a))
+
+S = 'str'; S1 = "str"; S2 = '''str'''; S3 = """str"""
+
+print(S)
+print(S1)
+print(S2)
+print(S3)
+
